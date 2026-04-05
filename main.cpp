e59110c470f42e0b50ff59165059b94c85bda3ef
neveroyatniy-bobr <nicant2007@gmail.com>
2026-04-05 22:16:32 +0300

    Первая версия мандельброта

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 #include <SFML/Graphics.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 static const int  WIDTH = 800;
 static const int HEIGHT = 600;
 static const int    BPP =  32;
+
+static const int MAX_ITERATIONS = 256;
+
+static const float MOVE_SENS = 0.05;
+static const float ZOOM_SENS = 1.05;
+
+int GetN(float cx, float cy) {
+    float zx = 0, zy = 0;
+    for (int i = 0; i < MAX_ITERATIONS; i++) {
+        //(zx + izy)^2 = zx^2 - zy^2 + 2*zx*zy*
+        float new_zx = zx*zx - zy*zy + cx;
+        float new_zy = 2*zx*zy + cy;
+        zx = new_zx;
+        zy = new_zy;
+
+        if (zx*zx + zy*zy >= 4.0f) {
+            return i;
+        }
+    }
+
+    return 255;
+}
 
 int main() {
     sfRenderWindow* window = sfRenderWindow_create((sfVideoMode){WIDTH, HEIGHT, BPP}, "Random pixels", sfClose, NULL);
@@ -18,26 +41,42 @@ int main() {
     sfClock* clock = sfClock_create();
     srand(time(NULL));
 
+    float x_pos = 0, y_pos = 0;
+    float zoom = 1;
+
     while (sfRenderWindow_isOpen(window)) {
         sfEvent event;
         while (sfRenderWindow_pollEvent(window, &event)) {
             if (event.type == sfEvtClosed) {
                 sfRenderWindow_close(window);
             }
-        }
-        
-        if (sfTime_asSeconds(sfClock_getElapsedTime(clock)) >= 1.0f) {
-            for (int p_i = 0; p_i < WIDTH * HEIGHT; p_i++) {
-                sfColor* pixel = pixels + p_i;
-                pixel->r = rand() % 256;
-                pixel->g = rand() % 256;
-                pixel->b = rand() % 256;
-                pixel->a = 255;
+            if (event.type == sfEvtKeyPressed) {
+                if (event.key.code == sfKeyEscape ) sfRenderWindow_close(window);
+                if (event.key.code == sfKeyW      ) y_pos += MOVE_SENS / zoom;
+                if (event.key.code == sfKeyS      ) y_pos -= MOVE_SENS / zoom;
+                if (event.key.code == sfKeyA      ) x_pos -= MOVE_SENS / zoom;
+                if (event.key.code == sfKeyD      ) x_pos += MOVE_SENS / zoom;
+                if (event.key.code == sfKeyE      ) zoom *= ZOOM_SENS;
+                if (event.key.code == sfKeyQ      ) zoom /= ZOOM_SENS;
             }
-
-            sfTexture_updateFromPixels(texture, (sfUint8*)pixels, WIDTH, HEIGHT, 0, 0);
-            sfClock_restart(clock);
         }
+
+        for (int p_i = 0; p_i < WIDTH * HEIGHT; p_i++) {
+            sfColor* pixel = pixels + p_i;
+            float cx = x_pos + ((float)(p_i%WIDTH) / (float)HEIGHT - 1.33f/2) / zoom       ;
+            float cy = y_pos + ((float)(p_i/WIDTH) / (float)HEIGHT -  1.0f/2) / zoom * (-1);
+
+            int n = GetN(cx, cy);
+            pixel->r = n *  7 % 255;
+            pixel->g = n * 11 % 255;
+            pixel->b = n * 17 % 255;
+            pixel->a = 255;
+        }
+
+        sfTexture_updateFromPixels(texture, (sfUint8*)pixels, WIDTH, HEIGHT, 0, 0);
+        printf("%d\n", (int)(1 / sfTime_asSeconds(sfClock_getElapsedTime(clock))));
+        sfClock_restart(clock);
+
 
         sfRenderWindow_drawSprite(window, sprite, NULL);
         sfRenderWindow_display(window);
